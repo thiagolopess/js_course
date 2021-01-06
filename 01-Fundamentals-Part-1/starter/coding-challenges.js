b8c310dd45d0ecf8cb1bf9b810d32c538b30e748
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-01-06 20:53:19 -0300

    Resolved coding challenge number 3

--- 01-Fundamentals-Part-1/starter/coding-challenges.js
+++ 01-Fundamentals-Part-1/starter/coding-challenges.js
@@ -14,3 +14,19 @@ if (markHigherBMI) {
     console.log(`John's BMI (${johnBMI}) is higher than Marks's (${markBMI}).`)
 }
 
+console.log("--------------------------------\nCoding Challenge 3\n--------------------------------")
+
+const dolphinsScore = (100 + 95 + 100) / 3;
+const koalasScore = (100 + 95 + 100) / 3;
+
+if (dolphinsScore > 100 || koalasScore > 100) {
+    if (dolphinsScore > koalasScore) {
+        console.log(`Dolphins are the winners! Score: ${dolphinsScore}`);
+    } else if (koalasScore > dolphinsScore) {
+        console.log(`Koalas are the winners! Score: ${koalasScore}`);
+    } else {
+        console.log(`It's a draw! Score: ${dolphinsScore}`);
+    }
+} else {
+    console.log(`Both of the teams didn't get the minimum 100 average score.\nDolphins Score: ${dolphinsScore}\nKoalas Score: ${koalasScore}`);
+}
