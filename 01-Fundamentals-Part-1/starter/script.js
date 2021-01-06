abbe02d2d9401a6031d6bc3778965c1557b8b35c
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-01-05 22:29:45 -0300

    Learning about data types in JS

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,2 +1,39 @@
+// Hello World
+
+/*
 let hw = 'hello world';
 console.log(hw);
+*/
+
+// Primitive data types
+
+// Number:
+let age = 32;
+
+// String
+let firstName = 'Thiago';
+
+// Boolean
+let fullAge = true;
+
+// Undefined, Null, Symbol and BigInt
+
+let jsIsFun = true;
+console.log(jsIsFun);
+
+// Check the type of a value
+console.log(typeof jsIsFun);
+console.log(typeof 20);
+console.log(typeof 'Hello');
+
+// Dynamic Typing
+jsIsFun = 1;
+console.log(typeof jsIsFun);
+
+// Undefined
+let year;
+console.log(year);
+
+// NULL problem
+console.log(typeof null);
+
