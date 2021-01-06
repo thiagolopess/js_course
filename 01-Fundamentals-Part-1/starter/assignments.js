2d9dc9880c414d96bf577dde4069bbef86928c58
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-01-06 20:13:24 -0300

    Added the 9th assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,12 +1,12 @@
 console.log("--------------------------------\nAssignments\n--------------------------------")
 
-/////////////// LECTURE: Values and Variables
+/////////////// 1- LECTURE: Values and Variables
 const country = 'Brazil', continent = 'South America';
 let population = 200000000;
 
 console.log(country, continent, population);
 
-/////////////// LECTURE: Data Types
+/////////////// 2- LECTURE: Data Types
 const isIsland = false;
 let language;
 
@@ -15,7 +15,7 @@ console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
 
-/////////////// LECTURE: let, const and var
+/////////////// 3- LECTURE: let, const and var
 language = 'portuguese';
 console.log(language);
 
@@ -23,7 +23,7 @@ console.log(language);
 isIsland = true;
 */
 
-/////////////// LECTURE: Basic Operators
+/////////////// 4- LECTURE: Basic Operators
 console.log(population / 2);
 population++;
 console.log(population);
@@ -34,18 +34,18 @@ let description = country + ' is in ' + continent + ' and its ' + population + '
 
 console.log(description);
 
-/////////////// LECTURE: Strings and Template Literals
+/////////////// 5- LECTURE: Strings and Template Literals
 description = `${country} is in ${continent} and its ${population} people speak ${language}`;
 console.log(description);
 
-/////////////// LECTURE: Taking Decisions: if / else Statements
+/////////////// 6- LECTURE: Taking Decisions: if / else Statements
 if (population > 33000000) {
     console.log(`${country}'s population is above average.`);
 } else {
     console.log(`${country}'s population is ${33000000 - population} below average`);
 }
 
-/////////////// LECTURE: Type Conversion and Coercion
+/////////////// 7- LECTURE: Type Conversion and Coercion
 
 console.log('9' - '5'); // 4 int
 console.log('19' - '13' + '17'); // 617 string
@@ -53,9 +53,9 @@ console.log('19' - '13' + 17); // 23 int
 console.log('123' < 57); // false bool
 console.log(5 + 6 + '4' + 9 - 4 - 2); // 1143 int
 
-/////////////// LECTURE: Equality Operators: == vs. ===
+/////////////// 8- LECTURE: Equality Operators: == vs. ===
 
-const numNeighbours = Number(prompt('How many neighbour countries does your country have?'));
+const numNeighbours = 0 //Number(prompt('How many neighbour countries does your country have?'));
 
 if (numNeighbours === 1) {
     console.log('Only 1 border!');
@@ -65,3 +65,12 @@ if (numNeighbours === 1) {
     console.log('No borders');
 }
 
+/////////////// 9- LECTURE: Logical Operators
+
+const speakEnglish = true;
+
+if (speakEnglish && population < 50000000 && !isIsland) {
+    console.log(`You should live in ${country}!`);
+} else {
+    console.log(`${country} does not meet your criteria :(`);
+}
