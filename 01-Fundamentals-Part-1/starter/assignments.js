87b2e9d1ad057967820487238a9c12136d9524c3
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-01-06 08:32:18 -0300

    Added the 3rd assignment

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,14 +1,26 @@
+console.log("--------------------------------\nAssignments\n--------------------------------")
+
 // LECTURE: Values and Variables
-let country = 'Brazil', continent = 'South America', population = '200000000';
+const country = 'Brazil', continent = 'South America';
+let population = '200000000';
 
 console.log(country, continent, population);
 
 // LECTURE: Data Types
-let isIsland = false;
+const isIsland = false;
 let language;
 
 console.log(typeof isIsland);
 console.log(typeof population);
 console.log(typeof country);
 console.log(typeof language);
+
+// LECTURE: let, const and var
+language = 'portuguese';
+console.log (language);
+
+/* invalid
+isIsland = true;
+*/
+
 
