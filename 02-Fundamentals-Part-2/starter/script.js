9006f8ff98788d5810aa65536c0557ec897114cf
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-01-07 09:28:12 -0300

    Learning about functions

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,24 @@
+// Strict Mode
+'use strict';
+
+/////////////// Functions
+
+function logger() {
+    console.log('My name is Thiago');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
