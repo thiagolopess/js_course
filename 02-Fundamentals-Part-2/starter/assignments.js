0ef299c8467d0c46647da0c7b85577f70af7e0c0
thiagolopess <thiago.lopes.santos.tls@gmail.com>
2021-01-07 10:22:42 -0300

    Added the 2nd assignment

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -4,7 +4,6 @@
 console.log("--------------------------------\nAssignments\n--------------------------------")
 
 /////////////// 1- LECTURE: Functions
-
 function describeCountry(country, population, capitalCity) {
     return `${country} has ${population} million habitants and its capital city is ${capitalCity}.`;
 }
@@ -14,3 +13,26 @@ const country2 = describeCountry('USA', '300', 'Washington, D.C');
 const country3 = describeCountry('Canada', '37', 'Ottawa');
 
 console.log(`${country1}\n${country2}\n${country3}`);
+
+/////////////// 2- LECTURE: Function Declarations vs. Expressions
+function percentageOfWorld1(population) {
+    return population * 100 / 7900;
+}
+
+const brazil = percentageOfWorld1(200);
+console.log(brazil);
+const usa = percentageOfWorld1(300);
+console.log(usa);
+const canada = percentageOfWorld1(37);
+console.log(canada);
+
+const percentageOfWorld2 = function (population) {
+    return population * 100 / 7900;
+}
+
+const brazil1 = percentageOfWorld2(200);
+console.log(brazil1);
+const usa1 = percentageOfWorld2(300);
+console.log(usa1);
+const canada1 = percentageOfWorld2(37);
+console.log(canada1);
